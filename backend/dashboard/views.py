735d978cbc3e86f45369d155c62f1a1953bcc95b
Aashigasree <aashigasree@gmail.com>
2026-07-23 16:05:34 +0530

    Enhance dashboard with context data and refactor templates for improved structure

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
 def dashboard(request):
-    return render(request, "dashboard.html")
+
+    context = {
+        "page_title": "Dashboard",
+
+        # Dashboard Statistics
+        "total_hosts": 0,
+        "online_hosts": 0,
+        "logs_today": 0,
+        "critical_alerts": 0,
+        "open_incidents": 0,
+        "ai_analyses": 0,
+    }
+
+    return render(request, "dashboard.html", context)
